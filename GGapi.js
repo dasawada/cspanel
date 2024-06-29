59635358d9c1b07b2e142f08e97af0fae25ca84c
dasawada <132549725+dasawada@users.noreply.github.com>
2024-06-29 18:42:14 +0800

    Delete GGapi.js

--- GGapi.js
+++
@@ -1,3 +0,0 @@
-const sheetId = "1FcjzaPWepGLRwdwyyefvZs_HEXhC168MircYGqpV9eQ";
-const apiKey = "AIzaSyAACAPVwRkK2Ii1nc8oJ8q0ha1ZF3gHlQU";
-const endpoint = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/顧問組別清單?key=${apiKey}`;
