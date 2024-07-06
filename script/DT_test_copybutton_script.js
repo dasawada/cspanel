5b320bd34bc957e8d2c71841a1cb4484e1d3d44f
dasawada <yuusuke.hamasaki@gmail.com>
2024-07-06 15:27:30 +0800

    06

--- script/DT_test_copybutton_script.js
+++ script/DT_test_copybutton_script.js
@@ -1,9 +1,13 @@
 document.getElementById('copyReportButton').addEventListener('click', function() {
     const outputContent = document.getElementById('output_content');
-    copyToClipboard(outputContent);
+    if (outputContent.textContent.trim() === "") {
+        showCopyError();
+    } else {
+        DT_copyToClipboard(outputContent);
+    }
 });
 
-function copyToClipboard(element) {
+function DT_copyToClipboard(element) {
     const range = document.createRange();
     range.selectNodeContents(element);
     const selection = window.getSelection();
@@ -11,20 +15,38 @@ function copyToClipboard(element) {
     selection.addRange(range);
 
     try {
-        document.execCommand('copy');
-        const copyButton = document.getElementById('copyReportButton');
-        copyButton.textContent = '已複製！';
-        copyButton.style.backgroundColor = 'green';
-        copyButton.style.color = 'white';
+        const successful = document.execCommand('copy');
+        if (successful) {
+            const copyButton = document.getElementById('copyReportButton');
+            copyButton.textContent = '已複製！';
+            copyButton.style.backgroundColor = 'green';
+            copyButton.style.color = 'white';
 
-        setTimeout(() => {
-            copyButton.textContent = '複製報告';
-            copyButton.style.backgroundColor = '';
-            copyButton.style.color = '';
-        }, 2000);
+            setTimeout(() => {
+                copyButton.textContent = '複製報告';
+                copyButton.style.backgroundColor = '';
+                copyButton.style.color = '';
+            }, 2000);
+        } else {
+            console.error('複製失敗');
+        }
     } catch (err) {
         console.error('複製失敗', err);
     }
 
     selection.removeAllRanges();
 }
+
+function showCopyError() {
+    const copyButton = document.getElementById('copyReportButton');
+    copyButton.textContent = '無報告可複製';
+    copyButton.style.backgroundColor = 'red';
+    copyButton.style.color = 'white';
+
+    setTimeout(() => {
+        copyButton.textContent = '複製報告';
+        copyButton.style.backgroundColor = '';
+        copyButton.style.color = '';
+    }, 2000);
+}
+
