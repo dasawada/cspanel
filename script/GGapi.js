3bd364601cb4d48613548f9008be7dc7e68ef884
dasawada <yuusuke.hamasaki@gmail.com>
2025-03-23 21:16:06 +0800

    323

--- script/GGapi.js
+++
@@ -1,3 +0,0 @@
-const sheetId = "15TsK4mB_zfH6SGqTvUe1AOYzwQfE90Z8gN1Gf5tiYLU";
-const apiKey = "AIzaSyCozo2rhMeVsjLB2e3nlI9ln_sZ4fIdCSw";
-const endpoint = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/wtf?key=${apiKey}`;
